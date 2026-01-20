40b775ae48e0660998233b00e11ed30b47cad207
bruno <bruno.zilio31@gmail.com>
2026-01-20 09:47:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "#components/Navbar.jsx";
+
+const App = () => {
+    return (
+        <div>
+           <main>
+               <Navbar/>
+           </main>
+        </div>
+    )
+}
+export default App
