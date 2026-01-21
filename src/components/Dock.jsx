94620f4b746f64a707b68a78d07e701398d02a21
bruno <bruno.zilio31@gmail.com>
2026-01-21 08:30:34 +0100

    implementing dock components

---
+++ src/components/Dock.jsx
@@ -0,0 +1,40 @@
+gimport React, {useRef} from 'react';
+import {dockApps} from "#constants/index.js";
+import {Tooltip} from "react-tooltip";
+
+const Dock = () => {
+    const dockRef = useRef(null);
+
+    const toggleApp = (app) => {
+
+    }
+
+    return (
+    <section id="dock">
+        <div ref={dockRef} className="dock-container">
+            {dockApps.map(({id, name, icon, canOpen}) => (
+                <div key={id} className="relative flex justify-center ">
+                    <button
+                        type="button"
+                        className="dock-icon"
+                        aria-label={name}
+                        data-tooltip-id="dock-tooltip"
+                        data-tooltip-content={name}
+                        data-tooltip-delay-show ={150}
+                        disabled={!canOpen}
+                        onClick={() => toggleApp({ id, canOpen })}
+                    >
+                        <img src={`/images/${icon}`}
+                         alt={name}
+                         loading="lazy"
+                         className={canOpen ?  '' : "opacity-50"}
+                        />
+                    </button>
+                </div>
+            ))}
+        </div>
+    </section>
+    );
+};
+
+export default Dock
