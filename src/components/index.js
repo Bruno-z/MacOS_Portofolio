6132bad5fddf9d3552394755acb3f6b5772c23f8
bruno <bruno.zilio31@gmail.com>
2026-01-29 11:21:47 +0100

    window wrapper component logic in a hoc

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
 import Dock from "#components/Dock.jsx";
+import WindowControls from "#components/WindowControls.jsx";
 
 
-export {Welcome, Navbar, Dock}
+export {Welcome, Navbar, Dock, WindowControls}
