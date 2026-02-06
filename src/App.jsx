798b873c5eae4451bf8d8d62b735a1a64a85bee6
bruno <bruno.zilio31@gmail.com>
2026-02-06 08:58:14 +0100

    social, gallery, photos, and text are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {Navbar, Welcome, Dock} from "#components"
 import  { Draggable} from "gsap/Draggable";
-import {Terminal} from "#windows/index.js";
+import {Safari, Terminal, Resume, Finder, Text,ImageWindowContent, Contact, Gallery} from "#windows/index.js";
 import gsap from "gsap";
 
 
@@ -15,6 +15,13 @@ const App = () => {
                <Dock/>
 
                <Terminal/>
+               <Safari/>
+               <Resume/>
+               <Finder/>
+               <Text/>
+               <ImageWindowContent/>
+               <Contact/>
+               <Gallery/>
            </main>
         </div>
     )
